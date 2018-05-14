de974f62427b2c871ee5fcb9ab5c6179574ea014
Max Beyer <inflatableanchor@gmail.com>
2018-05-13 21:36:43 -0500

    Class & Test creation

---
+++ src/coinsTest/CoinsTest.java
@@ -0,0 +1,14 @@
+package coinsTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CoinsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
