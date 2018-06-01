6d87908b26158e83000f0569577c5ea841c9d5a8
Max Beyer <inflatableanchor@gmail.com>
2018-06-01 13:35:48 -0500

    added coinsInserted method

--- src/vendingMachine/CoinSlot.java
+++ src/vendingMachine/CoinSlot.java
@@ -11,6 +11,7 @@ public class CoinSlot {
 	private static CoinIdentifierObject coinIdentifierObject = new CoinIdentifierObject();
 	private static double currentAmount = 0;
 	private List<Coin> coinReturn = new ArrayList<Coin>();
+	private static List<Coin> coinsInserted = new ArrayList<Coin>();
 
 	public double getCurrentAmount() {
 		return currentAmount;
@@ -24,6 +25,14 @@ public class CoinSlot {
 		return coinReturn;
 	}
 
+	public static List<Coin> getCoinsInserted() {
+		return coinsInserted;
+	}
+
+	public static void setCoinsInserted(List<Coin> coinsInserted) {
+		CoinSlot.coinsInserted = coinsInserted;
+	}
+
 	public Coin identifyCoin(Coin coin) {
 		Coin identifiedCoin = coinIdentifierObject.identifyCoin(coin);
 		return identifiedCoin;
@@ -33,6 +42,10 @@ public class CoinSlot {
 		//make new coin object every time, since every coin is different 
 		Coin insertedCoin = coinIdentifierObject.identifyCoin(coin);
 		if(insertedCoin.isValid()) {
+			coinsInserted.add(insertedCoin);
+			//setting the current amount this way is faster than looping through the inserted coins and adding them
+			//HOWEVER, perhaps this is bad practice since currentAmount and coinsInserted SHOULD be dependent on eachother
+			//right now they are independent
 			setCurrentAmount(getCurrentAmount() + insertedCoin.getValue());
 		} else {
 			coinReturn.add(insertedCoin);
